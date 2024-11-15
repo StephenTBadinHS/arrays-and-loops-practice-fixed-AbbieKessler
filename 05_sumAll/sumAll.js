329501415fa939d3654f692516f5cac7354b352f
Kessler <25KesslerA@badinhs.org>
2024-11-14 21:12:31 -0500

    adds up integers between 1 and 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function() {
-
+    min = 1, max = 4, sum = 0;
+    for (int = min; i <= max; i++) {
+        sum += i;
+        'reutrn';
+    }
 };
 
 // Do not edit below this line
